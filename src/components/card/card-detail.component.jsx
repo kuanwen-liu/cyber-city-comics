c7eea6215836fe4d549e1a264cdb02fbeb9f1b77
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2021-10-23 21:41:34 -0400

    add countAPI to count every comic that has been view on site

--- src/components/card/card-detail.component.jsx
+++ src/components/card/card-detail.component.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import "./card-detail.styles.css";
 
 const CardDetail = (props) => {
+  const [numberOfVisited, setNumberOfVisited] = useState();
+
+  // useParams to get the url parameter
+  const paras = useParams();
+  const paraNumber = paras.comicnumber;
+
+  // fetch CountAPI to count every comic that has been view on my website
+  useEffect(() => {
+    const counter = async () => {
+      try {
+        const response = await fetch(
+          `https://api.countapi.xyz/update/comic${paraNumber}/comic${paraNumber}/?amount=1`
+        );
+
+        // Check if the api does not return the value then create a new one
+        if (!response.ok) {
+          await fetch(
+            `https://api.countapi.xyz/create?namespace=comic${paraNumber}&key=comic${paraNumber}&value=0`
+          );
+          counter();
+        }
+
+        const num = await response.json();
+
+        setNumberOfVisited(num.value);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    counter();
+  }, [paraNumber]);
+
   // Parse transcript to more readable format
   const transcript = props.comicData.transcript
     .split("\n")
@@ -11,7 +44,10 @@ const CardDetail = (props) => {
   return (
     <div className="card-detail">
       <h1>{props.comicData.alt}</h1>
-      <h5>Date: {props.createdDate}</h5>
+      <h3>
+        <span>Date: {props.createdDate}</span>
+        <span>Viewed: {numberOfVisited}</span>
+      </h3>
       <img src={props.comicData.img} alt={props.comicData.alt} />
       <div>{transcript}</div>
     </div>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./header.styles.css";
 import Navigation from "./navigation.component";
@@ -8,7 +9,9 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo">
-        <img src={logo} alt="logo" />
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
         <div>
           <h1>Cyber City Comics</h1>
         </div>
